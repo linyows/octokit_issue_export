b40c9b92f695057785642f6024ba1c1c62f77a6d
linyows <linyows@gmail.com>
2013-10-29 14:29:38 +0900

    add sleep in order to avoid the limit api request

--- lib/octokit_issue_export/client.rb
+++ lib/octokit_issue_export/client.rb
@@ -3,5 +3,20 @@ require 'octokit/client/issue_export'
 module Octokit
   class Client
     include Octokit::Client::IssueExport
+    alias :original_request :request
+
+    def _request_stopper
+      return if last_response.nil?
+      return unless Octokit.rate_limit.remaining.zero?
+
+      minutes = (rate_limit.resets_in + 5)/60
+      puts "=> Rate limit! Please wait #{minutes} minutes(#{rate_limit.resets_at})..."
+      sleep minutes
+    end
+
+    def request(method, path, data, options = {})
+      _request_stopper
+      original_request(method, path, data, options = {})
+    end
   end
 end
